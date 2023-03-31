b24247865e6a934e080aa5eaed64c2bc7be5bf6a
Arivarasan Ramasamy <arivarasanram@gmail.com>
2023-03-30 20:33:27 -0400

    issue fix

--- mapper.py
+++ mapper.py
@@ -7,6 +7,7 @@ count = 0
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
+
     # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words
